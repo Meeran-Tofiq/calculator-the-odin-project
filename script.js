34b4bcd4172ee7146079af531c50d96b20180647
Meeran <101665499+Meeran420@users.noreply.github.com>
2022-12-23 14:13:27 +0300

    Add functions for basic operations

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    return +a + +b;
+}
+
+function sub(a, b) {
+    return +a - +b;
+}
+
+function mult(a, b) {
+    return +a * +b;
+}
+
+function divi(a, b) {
+    return +a / +b;
+}
+
+function pow(a, b) {
+    return Math.pow(+a, +b);
+}
