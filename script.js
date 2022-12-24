27897d29ad9606008b593ff8063fa05c364863ba
Meeran <101665499+Meeran420@users.noreply.github.com>
2022-12-24 13:26:05 +0300

    Add all buttons to variables

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let displayValue = "";
+let a = "";
+let b = "";
+
+const one = document.querySelector('#1');
+const two = document.querySelector('#2');
+const three = document.querySelector('#3');
+const four = document.querySelector('#4');
+const five = document.querySelector('#5');
+const six = document.querySelector('#6');
+const seven = document.querySelector('#7');
+const eight = document.querySelector('#8');
+const nine = document.querySelector('#9');
+const zero = document.querySelector('#0');
+
+const add = document.querySelector('#add');
+const sub = document.querySelector('#sub');
+const mult = document.querySelector('#mult');
+const divi = document.querySelector('#divi');
+const pow = document.querySelector('#pow');
+const equals = document.querySelector('#equals');
+
+const clear = document.querySelector('#clear');
+
 function add(a, b) {
     return +a + +b;
 }
