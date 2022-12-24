08d8b3b14ecbdec738948aee57f27f579c8006a3
Meeran <101665499+Meeran420@users.noreply.github.com>
2022-12-24 13:42:52 +0300

    Add events for cicking numbers and clear

--- script.js
+++ script.js
@@ -2,23 +2,25 @@ let displayValue = "";
 let a = "";
 let b = "";
 
-const one = document.querySelector('#1');
-const two = document.querySelector('#2');
-const three = document.querySelector('#3');
-const four = document.querySelector('#4');
-const five = document.querySelector('#5');
-const six = document.querySelector('#6');
-const seven = document.querySelector('#7');
-const eight = document.querySelector('#8');
-const nine = document.querySelector('#9');
-const zero = document.querySelector('#0');
+const screen = document.querySelector("#screen");
 
-const add = document.querySelector('#add');
-const sub = document.querySelector('#sub');
-const mult = document.querySelector('#mult');
-const divi = document.querySelector('#divi');
-const pow = document.querySelector('#pow');
-const equals = document.querySelector('#equals');
+const one = document.querySelector('#n1');
+const two = document.querySelector('#n2');
+const three = document.querySelector('#n3');
+const four = document.querySelector('#n4');
+const five = document.querySelector('#n5');
+const six = document.querySelector('#n6');
+const seven = document.querySelector('#n7');
+const eight = document.querySelector('#n8');
+const nine = document.querySelector('#n9');
+const zero = document.querySelector('#n0');
+
+const addOp = document.querySelector('#add');
+const subOp = document.querySelector('#sub');
+const multOp = document.querySelector('#mult');
+const diviOp = document.querySelector('#divi');
+const powOp = document.querySelector('#pow');
+const equalsOp = document.querySelector('#equals');
 
 const clear = document.querySelector('#clear');
 
@@ -43,15 +45,37 @@ function pow(a, b) {
 }
 
 function operate(a, b, oper) {
-    if(oper === '+') {
+    if (oper === '+') {
         return add(a, b)
-    } else if(oper === '-') {
+    } else if (oper === '-') {
         return sub(a, b)
-    } else if(oper === '*') {
+    } else if (oper === '*') {
         return mult(a, b)
-    } else if(oper === '/') {
+    } else if (oper === '/') {
         return divi(a, b)
-    } else if(oper === '^') {
+    } else if (oper === '^') {
         return pow(a, b)
     }
+}
+
+one.addEventListener('click', () => changeDisplayValue(1));
+two.addEventListener('click', () => changeDisplayValue(2));
+three.addEventListener('click', () => changeDisplayValue(3));
+four.addEventListener('click', () => changeDisplayValue(4));
+five.addEventListener('click', () => changeDisplayValue(5));
+six.addEventListener('click', () => changeDisplayValue(6));
+seven.addEventListener('click', () => changeDisplayValue(7));
+eight.addEventListener('click', () => changeDisplayValue(8));
+nine.addEventListener('click', () => changeDisplayValue(9));
+zero.addEventListener('click', () => changeDisplayValue(0));
+
+clear.addEventListener('click', () => clearDisplay());
+
+function changeDisplayValue(a) {
+    screen.textContent += a;
+}
+
+function clearDisplay() {
+    console.log("i ben presed")
+    screen.textContent = "";
 }
